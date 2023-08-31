b2a8f8a6e44c6d307230030349ee791649f7ed8b
solbot <poseisol@outlook.com>
2023-08-31 09:29:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useWeb3React } from "@web3-react/core";
+import "./App.css";
 
 function App() {
+  const { isActive, chainId, account, connector } = useWeb3React();
+  console.log(isActive, chainId, account, connector);
+  useEffect(() => {
+    connector.connectEagerly?.();
+  }, [connector]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {account ? (
+        <button
+          onClick={() => {
+            if (connector?.deactivate) {
+              connector.deactivate();
+            } else {
+              connector.resetState();
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Disconnect
+        </button>
+      ) : (
+        <button onClick={() => connector.activate()}>Metamask Connect</button>
+      )}
     </div>
   );
 }
